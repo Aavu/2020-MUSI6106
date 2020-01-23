1dadaca589ea731f6970e4c8eee3f2455e9f9f5c
aavu <aavu@gatech.edu>
2020-01-22 22:49:12 -0500

    Added test files

--- test/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from scipy.io.wavfile import read as wavread
-
-x = np.loadtxt("out.txt")
-fs, audio = wavread("test.wav")
-audio = audio / (2 ** 15)
-print("Error:", np.mean(np.abs(x - audio)))
